9674cfbac345295d07dcf9729c5746bf77562a89
ana.dias <anadiasacd690@gmail.com>
2021-11-13 18:04:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+        TextView  texto = findViewById(R.id.textResultado);
+        int numero = new Random().nextInt(14);
+        texto.setText("" + numero);
+    }
+
+
+}
